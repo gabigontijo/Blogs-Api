986a24429489a520abec2778c846623b3594dba3
Gabriela Gontijo <dev.gabigontijo@gmail.com>
2023-03-06 22:15:01 +0000

    req 02

---
+++ src/models/User.js
@@ -0,0 +1,21 @@
+module.exports = (sequelize, DataTypes) => {
+    const User = sequelize.define('User', {
+      id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+      displayName: DataTypes.STRING,
+      email: { type: DataTypes.STRING, unique: true, allowNull: false  },
+      password: { type: DataTypes.STRING, allowNull: false },
+      image: DataTypes.STRING,
+    },
+    {
+      timestamps: false, // remove a obrigatoriedade de utilizar os campos `createdAt` e `updatedAt`
+      tableName: 'users',
+      underscored: true,
+    });
+
+    User.associate = (models) => {
+      User.hasOne(models.BlogPost,
+        { foreignKey: 'userId', as: 'blogPosts' });
+    };
+
+    return User;
+  };
